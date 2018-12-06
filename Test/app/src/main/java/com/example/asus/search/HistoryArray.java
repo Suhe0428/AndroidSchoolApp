e0fddde46bad5e56ba1c110b9f27ec92904d842a
Suhe <wuxiansheng0428@163.com>
2018-12-06 14:45:22 +0800

    删除了Test

--- Test/app/src/main/java/com/example/asus/search/HistoryArray.java
+++
@@ -1,8 +0,0 @@
-package com.example.asus.search;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class HistoryArray {
-    public static List<String> historyArray=new ArrayList<>();
-}
